43bddcc7cd93a454af86192b4c7777d49f62e069
eslam <emaree86@gmail.com>
2023-03-21 11:40:17 -0700

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,18 @@
-//#include <stdio.h>
 #include "main.h"
 
 /**
- * main - prints all possible combinations of two two-digit numbers
+ * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("_putchar");
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
     putchar('\n');
-
 }
-
